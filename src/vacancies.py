88d1160cda0439adc7b8b0254c53bc5e8b8267ac
SergeyMediv <bochkarev.s.e@yandex.ru>
2024-01-15 23:31:34 +0300

    создать класс для работы с вакансиями

---
+++ src/vacancies.py
@@ -0,0 +1,39 @@
+class Vacancy:
+    def __init__(
+            self,
+            vacancy_name: str,
+            salary_from: int,
+            salary_to: int,
+            vacancy_skills: str,
+            vacancy_url: str,
+            vacancy_currency: str,
+            vacancy_duties: str
+    ):
+        self.vacancy_name = vacancy_name
+        self.salary_from = self.validate_salary(salary_from)
+        self.salary_to = self.validate_salary(salary_to)
+        self.salary_repr = self.salary_representation(salary_from, salary_to)
+        self.vacancy_currency = vacancy_currency
+        self.vacancy_skills = vacancy_skills
+        self.vacancy_duties = vacancy_duties
+        self.vacancy_url = vacancy_url
+
+    @staticmethod
+    def validate_salary(salary):
+        if salary is None:
+            return 0
+        return salary
+
+    def salary_representation(self, salary_from, salary_to):
+        return f'{self.validate_salary(salary_from)} - {self.validate_salary(salary_to)}'
+
+    def __gt__(self, other):
+        return self > other
+
+    def __lt__(self, other):
+        return self < other
+
+    def __eq__(self, other):
+        return self == other
+
+
